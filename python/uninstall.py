12fc5e1a90492a1c275dd77baa7050258e0aca96
mandorodriguez <mandorodriguez@gmail.com>
2011-09-01 17:20:07 +0000

    1. Added the python uninstall script.

---
+++ python/uninstall.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+"""
+
+"""
+
+import pdb
+import os
+import sys
+
+#
+# Since the current directory is part of sys.path
+# uninstall.py must check for it and remove it, otherwise
+# it will delete the neurospaces directory in the source
+# directory.
+#
+_this_directory =  os.path.dirname(os.path.abspath(__file__))
+
+try:
+
+    sys.path.remove(_this_directory)
+
+except ValueError, e:
+
+    print "The current directory is not in the system path"
+
+
+try:
+
+    sys.path.remove('.')
+
+except ValueError:
+
+    pass
+
+try:
+    
+    from neurospaces.packages import PackageManager
+
+except ImportError:
+
+    print "Neurospaces packages are not installed."
+    
+    sys.exit(0)
+
+package_manager = PackageManager(verbose=False)
+
+try:
+    
+    package_manager.Uninstall('developer')
+
+except Exception, e:
+
+    print "Can't uninstall developer: %s" % e
