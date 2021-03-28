49727cb2b0fa8f1e472d16285a61a936250f788a
Hugo Cornelis <hugo.cornelis@gmail.com>
2021-03-28 17:08:59 +0200

    workflow, bugfix: bash completion was implemented for the generic 'workflow' rather than the project specific 'neurospaces-workflow', fixed.

--- bin/neurospaces-bash-completion.sh
+++ bin/neurospaces-bash-completion.sh
@@ -1,7 +1,9 @@
 #/usr/bin/env bash
 _neurospaces_configuration_completions()
 {
-    COMPLETIONS=`workflow --bash-completion "$COMP_LINE" $COMP_CWORD $COMP_POINT | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g"`
+    #! note: include the project specific prefix in the program name for project specific completions
+
+    COMPLETIONS=`neurospaces-workflow --bash-completion "$COMP_LINE" $COMP_CWORD $COMP_POINT | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g"`
 
     COMPREPLY=($(compgen -W "$COMPLETIONS" -- "${COMP_WORDS[$COMP_CWORD]}"))
 }
@@ -12,13 +14,15 @@ complete -F _neurospaces_configuration_completions ./neurospaces-configuration
 
 _neurospaces_workflow_completions()
 {
-    COMPLETIONS=`workflow --bash-completion "$COMP_LINE" $COMP_CWORD $COMP_POINT | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g"`
+    #! note: include the project specific prefix in the program name for project specific completions
+
+    COMPLETIONS=`neurospaces-workflow --bash-completion "$COMP_LINE" $COMP_CWORD $COMP_POINT | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g"`
 
     COMPREPLY=($(compgen -W "$COMPLETIONS" -- "${COMP_WORDS[$COMP_CWORD]}"))
 }
 
-complete -F _neurospaces_workflow_completions workflow
-complete -F _neurospaces_workflow_completions ./workflow
+# complete -F _neurospaces_workflow_completions workflow
+# complete -F _neurospaces_workflow_completions ./workflow
 
 complete -F _neurospaces_workflow_completions neurospaces-workflow
 complete -F _neurospaces_workflow_completions ./neurospaces-workflow
